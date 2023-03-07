4337a932b2ce6042434ac5982e91c3e93c9a725a
JakubBernaciak <97189751+JakubBernaciak@users.noreply.github.com>
2023-03-07 18:14:41 +0100

    Add first implementation of #.Xk

--- lab2/python/lab0/main.py
+++ lab2/python/lab0/main.py
@@ -11,6 +11,21 @@ def my_printf(format_string,param):
                 param = param.swapcase()
                 print(param,end="")
                 shouldDo=False
+            elif format_string[idx] == '#' and format_string[idx+1] == '.':
+                i = 2
+                while(format_string[idx+i].isnumeric()):
+                    i+=1
+                if i == 2:
+                    format_string[idx]
+                    print(format_string[idx],end="")
+                elif format_string[idx+i] == 'k':
+                    number = int(format_string[idx + 2: idx + i])
+                    param = param.swapcase()
+                    print(param[0:number],end="")
+                    idx = idx + i + 2
+                    shouldDo=False
+                    continue
+
             else:
                 format_string[idx]
                 print(format_string[idx],end="")
