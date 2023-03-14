e54115a18ef46760361e300573edbd7704a139cb
JakubBernaciak <97189751+JakubBernaciak@users.noreply.github.com>
2023-03-14 17:49:48 +0100

    Add new test case for #.k and correct function

--- lab3/main.py
+++ lab3/main.py
@@ -3,18 +3,42 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
+    if len(format_string) == 0:
+        print("")
+        return
+    
+    is_found = False
+    for i in range(len(format_string)):
+        if format_string[i] == '#':
+            for j in range(i+1,len(format_string)):
+                if format_string[j].isnumeric() or format_string[j] in ['.','k']:
+                    if format_string[j] == 'k':
+                        beg_i = i
+                        end_i = j
+                        is_found = True
+                        break
+                else: break
+        if is_found: break
+    
+    if not is_found:
+        print(format_string)
+        return
+    param = param.swapcase()
+    
+    dot_i = format_string[beg_i:end_i].find('.')
+    if dot_i != -1:
+        if format_string[beg_i + dot_i +1].isnumeric():
+            new_len = int(format_string[beg_i + dot_i +1:end_i])
+            param = param[:new_len]
         else:
-            shouldDo=True
-    print("")
+            param = ""
+    else: dot_i = end_i - beg_i
+
+    if beg_i + 1 < beg_i + dot_i:
+        new_len = int(format_string[beg_i + 1: beg_i + dot_i])
+        param = ' ' * (new_len - len(param)) + param
+    print(format_string[:beg_i] + param + format_string[end_i + 1:])
+
 
 data=sys.stdin.readlines()
 
