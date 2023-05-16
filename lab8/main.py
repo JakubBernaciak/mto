200d1ed04942f13bf04751f561e8bab4ddd77826
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-05-16 17:27:43 +0200

    Copy code from lab7

--- lab8/main.py
+++ lab8/main.py
@@ -3,19 +3,28 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
-
+    if not format_string or "#j" not in format_string or not param:
+        print(format_string)
+        return
+    
+    is_negative = False
+    if param[0] == '-':
+        is_negative = True
+        param = param[1:]
+    
+    if not param.isnumeric():
+        print(format_string)
+        return
+    
+    param = str(hex(int(param)))[2:]
+    for letter in ["a","b","c","d","e","f"]:
+        param = param.replace(letter,chr(ord(letter)+6))
+    
+    if is_negative:
+        param = "-" + param
+    
+    print(format_string.replace("#j", param))
+        
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
