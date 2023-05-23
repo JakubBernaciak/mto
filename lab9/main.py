d6e8a18a92edd42ad371aa27cd261b13ed0872f1
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-05-23 17:26:00 +0200

    Add first implementation of printf

--- lab9/main.py
+++ lab9/main.py
@@ -3,19 +3,27 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
-
+    if not format_string:
+        print("")
+    if not param:
+        print(format_string)
+        
+    is_found = False
+    number = 0
+    for i in range(len(format_string)):
+        if format_string[i] == '#' and format_string [i+1] =='.':
+            for j in range(i+2,len(format_string)):
+                if format_string[j].isnumeric():
+                    continue
+                elif format_string[j] == 'h':
+                    number = int(format_string[i+2:j-1])
+                    is_found == True
+                else:
+                    break
+    if not is_found:
+        print(format_string)
+       
+       
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
