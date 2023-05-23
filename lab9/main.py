e143464fd38cd349747a0ec58bc5da577a043cba
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-05-23 17:42:24 +0200

    Add placing param into format string

--- lab9/main.py
+++ lab9/main.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def number_swap(number)
+def number_swap(number):
     return chr(ord('a') + int(number))
 
 def numberinator(param):
@@ -10,32 +10,42 @@ def numberinator(param):
     first_word = param.split('.')[0]
     for letter in first_word:
         result += number_swap(letter)
-    
+    return result
     
 
 def my_printf(format_string,param):
     if not format_string:
         print("")
-    if not param or not param.isnumeric():
+        return
+    if not param:
         print(format_string)
+        return
         
     is_found = False
     number = 0
+    beg = 0
+    end = 0
     for i in range(len(format_string)):
         if format_string[i] == '#' and format_string [i+1] =='.':
-            for j in range(i+2,len(format_string)):
+            for j in range(i+2,len(format_string),1):
                 if format_string[j].isnumeric():
                     continue
                 elif format_string[j] == 'h':
-                    number = int(format_string[i+2:j-1])
-                    is_found == True
+                    beg = i
+                    end = j + 1
+                    number = int(format_string[i+2:j])
+                    is_found = True
                 else:
                     break
     if not is_found:
         print(format_string)
+        return
     
+    result = numberinator(param)
     
-       
+    print(format_string[:beg] + result + format_string[end:])
+        
+    
        
 data=sys.stdin.readlines()
 
