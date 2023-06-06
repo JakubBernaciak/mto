b129a3b09baea819ef6dfe1e7de9185e38b21964
Jakub Bernaciak <235830@edu.p.lodz.pl>
2023-06-06 17:19:06 +0200

    Add my_printf

--- lab11/main.py
+++ lab11/main.py
@@ -2,8 +2,39 @@
 
 import sys
 
+def convert(number):
+    res = ""
+    for i in range(len(number)):
+        if number[len(number)-1-i] == "1":
+            res =  chr(ord("a")+(i%10)) + res
+        else:
+            res = "0" + res
+    return res
+    
+
 def my_printf(format_string,param):
-    print("")
+    if not format_string:
+        print("")
+        return
+    elif not "#b" in format_string or not param:
+        print(format_string)
+        return
+    
+    is_negative = False
+    if param[0] == '-':
+        param = param[1:]
+        is_negative = True
+    try:
+        number = convert(str(bin(int(param))[2:]))
+    except:
+        print(format_string)
+        return
+    
+    if is_negative:
+        number = "-" + number
+        
+    print(format_string.replace("#b", number))
+    
 
 data=sys.stdin.readlines()
 
